5976044d0a6f627dc85fe8107a85b82d9fc80048
pablo1795 <pablo.diaz1795@gmail.com>
2024-06-11 20:17:43 -0300

    initial installation
    install Next JS

---
+++ portfolio-frontend/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="h-screen flex flex-col items-center justify-center">
+      <h1 className="text-5xl">¡Hola Mundo!</h1>
+    </main>
+  );
+}
